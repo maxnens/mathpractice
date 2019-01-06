0c5a786241e33f6abec156556eb415a80c25a994
max ens <maxfieldens@gmail.com>
2019-01-06 16:10:47 -0700

    add addition

--- addition.py
+++ addition.py
@@ -5,16 +5,17 @@ start = time()
 score = 0
 elapsed = time() - start
 while elapsed < 60:
-    x = randint(1,16)
-    result = "wrong ({})".format(2 ** x)
+    x = randint(2,100)
+    y = randint(2,100)
+    result = "wrong"
     try:
-        answer = int(input("2^{} = ".format(x)))
-        if answer == (2 ** x):
+        answer = int(input("{} + {} = ".format(x,y)))
+        if answer == x + y:
             score += 1
             result = "good"
     except ValueError:
         result = "BAD"
     elapsed = time() - start
-    print("{:0.3f}] {} score: {}".format(elapsed, result, score))
+    print("{:0.3f}] {}: {}".format(elapsed, result, score))
 
 print("DONE! {} correct answers".format(score))
